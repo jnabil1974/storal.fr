a54373bff4bce73d23cecc88d491ebc50728ab85
Nabil <njlaiel@gmail.com>
2026-01-20 02:21:24 +0100

    chore: Redirect /kissimy to /products/kissimy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/kissimy',
+        destination: '/products/kissimy',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
