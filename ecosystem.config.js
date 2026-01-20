801583e2fb1815e637ee5f1b2c9ee6cfc94c29c4
Nabil <njlaiel@gmail.com>
2026-01-20 03:01:53 +0100

    sync: Final config with port 3001 and env file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,12 @@ module.exports = {
       name: 'storal-next',
       cwd: __dirname,
       script: 'node_modules/.bin/next',
-      args: 'start -p 3000',
+      args: 'start -p 3001',
       instances: 1,
       exec_mode: 'fork',
+      env_file: '.env',
       env: {
         NODE_ENV: 'production',
-        NEXT_PUBLIC_SITE_URL: 'https://storal.fr',
-        NEXT_PUBLIC_SUPABASE_URL: 'REPLACE_WITH_SUPABASE_URL',
-        SUPABASE_SERVICE_ROLE_KEY: 'REPLACE_WITH_SERVICE_ROLE_KEY',
-        STRIPE_PUBLIC_KEY: 'REPLACE_WITH_STRIPE_PUBLIC_KEY',
-        STRIPE_SECRET_KEY: 'REPLACE_WITH_STRIPE_SECRET_KEY'
       }
     }
   ]
