388340ee1e4392ce07e35384e52e143215424783
Nabil <njlaiel@gmail.com>
2026-01-24 10:39:59 +0100

    chore(favicon): add explicit head links for Safari

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.svg?v=2" type="image/svg+xml" sizes="any" />
+      <link rel="icon" href="/favicon-32.png?v=2" type="image/png" sizes="32x32" />
+      <link rel="icon" href="/favicon-16.png?v=2" type="image/png" sizes="16x16" />
+      <link rel="shortcut icon" href="/favicon.ico?v=2" />
+      <link rel="apple-touch-icon" href="/apple-icon.png?v=1" sizes="180x180" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1A2B4C" />
+    </>
+  );
+}
