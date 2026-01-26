7a5ff93d707b76253a085837016c53b8a2a9f4cc
Nabil <njlaiel@gmail.com>
2026-01-26 16:23:21 +0100

    chore(next): remove deprecated images.domains; rely on remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['qctnvyxtbvnvllchuibu.supabase.co'],
     remotePatterns: [
       {
         protocol: 'https',
