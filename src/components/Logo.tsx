e2747fc5749fd2dfa9642cf9f6b370ff7dc04db3
Nabil <njlaiel@gmail.com>
2026-02-21 12:40:00 +0100

    perf: optimiser le logo - reduction de 6.7Mo à 14Ko (WebP) et 209Ko (PNG)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,14 +3,17 @@ import Image from 'next/image';
 export default function Logo() {
   return (
     <div className="flex items-center">
-      <Image
-        src="/images/logo-storal.png"
-        alt="STORAL - Store, Protection Solaire, Menuiserie"
-        width={181}
-        height={73}
-        className="h-[57px] w-auto"
-        priority
-      />
+      <picture>
+        <source srcSet="/images/logo-storal.webp" type="image/webp" />
+        <Image
+          src="/images/logo-storal.png"
+          alt="STORAL - Store, Protection Solaire, Menuiserie"
+          width={181}
+          height={73}
+          className="h-[57px] w-auto"
+          priority
+        />
+      </picture>
     </div>
   );
 }
